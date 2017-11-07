6c28a90d72272ccad04d6c698ad6fb632b97c5c2
Evan Zlotnick <evan_zlotnick@yahoo.com>
2017-11-06 20:28:01 -0600

    initial commit

---
+++ handler.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'hello world',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
