53af46c93e7eb69dd9562c07536c138961b9b4cf
Evan Zlotnick <evan_zlotnick@yahoo.com>
2018-01-29 17:47:12 -0800

    demo for juan

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'hello world from sandbox'
+      message: 'hello juan from sandbox'
     }),
   };
 
