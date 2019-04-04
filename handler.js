73e3bb1b3eb4e08f64a9b510607a82e6320ad009
Evan Zlotnick <evan_zlotnick@yahoo.com>
2019-04-03 17:10:34 -0700

    the fourth time is the best

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'hello node8 take 3 from sandbox'
+      message: 'hello node8 take 4 from sandbox'
     }),
   };
 };
