088db519d7f03cdedea67fdc2e6fd83b7f6c54c3
Evan Zlotnick <evan_zlotnick@yahoo.com>
2019-04-03 17:01:42 -0700

    another version

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'hello node8 again from sandbox'
+      message: 'hello node8 take 3 from sandbox'
     }),
   };
 };
